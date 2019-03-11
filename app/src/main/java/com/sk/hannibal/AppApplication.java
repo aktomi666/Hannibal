d8070472756029e5c740a729bea3b8989d4c7408
liujia <liujia@olayc.cn>
2019-03-11 16:55:51 +0800

    fs

--- app/src/main/java/com/sk/hannibal/AppApplication.java
+++ app/src/main/java/com/sk/hannibal/AppApplication.java
@@ -1,15 +1,28 @@
 package com.sk.hannibal;
 
 import android.app.Application;
+import android.content.Context;
 
 import com.hannibal.scalpel.Hannibal;
+
+import java.net.MulticastSocket;
+
+import androidx.multidex.MultiDex;
+import androidx.multidex.MultiDexApplication;
 
 
 /**
  * Created by ola_sk on 2018/11/23.
  * Email: magicbaby810@gmail.com
  */
-public class AppApplication extends Application {
+public class AppApplication extends MultiDexApplication {
+
+
+    @Override
+    protected void attachBaseContext(Context base) {
+        super.attachBaseContext(base);
+        MultiDex.install(this);
+    }
 
     @Override
     public void onCreate() {
